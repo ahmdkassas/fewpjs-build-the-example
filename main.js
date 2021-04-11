9d93f579c771f6e0f0d8001496e8734531867dc1
ahmed <ahm.gmal@gmail.com>
2021-04-11 14:17:30 -0400

    Completed assignment

--- main.js
+++ main.js
@@ -4,6 +4,53 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const glyphs = document.getElementsByClassName("like-glyph");
+const divError = document.getElementById('modal');
+divError.className = 'hidden';
+
+const errorMessage = document.getElementById('modal-message');
+
+
+for (let i = 0; i < glyphs.length; i++) {
+  glyphs[i].innerHTML = EMPTY_HEART;
+  glyphs[i].classList.remove('activated-heart');
+}
+
+for (let i = 0; i < glyphs.length; i++) {
+
+  glyphs[i].addEventListener('click', event => {
+
+    const glyphSpan = event.target;
+    const glyphState = glyphSpan.classList.contains('activated-heart');
+
+    if (glyphState) {
+      glyphSpan.classList.remove('activated-heart');
+      glyphSpan.innerHTML = EMPTY_HEART;
+    }
+    else {
+      PostServer(glyphSpan);
+    }
+  });
+}
+
+
+
+errorMessage.innerHTML = '';
+
+function PostServer(glyphSpan) {
+  mimicServerCall()
+  .then( () => {
+    glyphSpan.innerHTML = FULL_HEART;
+    glyphSpan.classList.add('activated-heart');
+  })
+  .catch( e => {
+    divError.className = '';
+    errorMessage.innerHTML = e;
+    setTimeout(() => divError.className = 'hidden', 2000);
+  });
+}
+
+
 
 
 
